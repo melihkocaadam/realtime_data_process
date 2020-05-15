f52d1b9a49716b5dbd1ffca1ce03bdb16cadf5b8
melihkocaadam <melihkocaadam@gmail.com>
2020-05-15 10:05:34 +0300

    edit agents query

--- web_app.py
+++ web_app.py
@@ -84,7 +84,8 @@ def getAgentsData():
                             ) as mtbl
                         LEFT JOIN "agents" atbl
                             ON atbl.agent = mtbl.agent
-                            and atbl.sequence = mtbl.max_seq"""}
+                            and atbl.sequence = mtbl.max_seq
+                        WHERE COALESCE(atbl.status, '') not in ('Logout')"""}
     r = requests.post(url, data=json.dumps(param), headers=headers)
     result = r.text
     
