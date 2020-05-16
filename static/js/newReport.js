fb590aa3e70525bb5bfafc4526a6fa104f13acb7
melihkocaadam <melihkocaadam@gmail.com>
2020-05-16 23:01:23 +0300

    add debug loging

--- static/js/newReport.js
+++ static/js/newReport.js
@@ -78,6 +78,7 @@ function createHTML(jsonData, endPoint) {
     }
 
     var divContainer = document.getElementById("addTables");
+    conesole.log(typeof divContainer);
     var existChild = divContainer.getElementById(endPoint);
     if (existChild.id == endpoint) {
         divContainer.parentNode.removeChild(existChild);
