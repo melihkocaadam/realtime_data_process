bfa676ea797fd052e56ae2ed590b0bf6c4882b72
melihkocaadam <melihkocaadam@gmail.com>
2020-05-18 15:20:35 +0300

    edit

--- consumer.py
+++ consumer.py
@@ -1,12 +1,20 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, TopicPartition
 from datetime import datetime
 import json
 
+topic = 'agents'
 consumer = KafkaConsumer(
-    'agents',
+    topic=topic,
     client_id='local-consumer',
     auto_offset_reset='smallest',
+    iter_timeout=5,
     bootstrap_servers=['localhost:9092'])
+tp = TopicPartition(
+    topic=topic,
+    partition=0
+    )
+end_offset = consumer.end_offsets(tp)
+print(end_offset)
 
 jsonResult = []
 for message in consumer:
@@ -17,7 +25,9 @@ for message in consumer:
     msg_len = len(str(msg_json))
     print(msg_len, msg_json)
     jsonResult.append(msg_json)
-    
-    print(message.offset)
+    msg_offset = message.offset
+    print(msg_offset)
+    if msg_offset == end_offset:
+        break
 
 print(jsonResult)
