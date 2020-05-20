a32f065cd89c2e5044707d211f02ec8e85a4abf7
melihkocaadam <melihkocaadam@gmail.com>
2020-05-20 16:26:29 +0300

    edit

--- consumer.py
+++ consumer.py
@@ -23,7 +23,7 @@ for offset in end_oss:
     break
 print(end_offset)
 
-consumer.seek_to_beginning([tp])
+consumer.seek_to_beginning(tp)
 exist_offset = consumer.position(tp)
 print(exist_offset)
 
