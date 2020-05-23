c896129ccbe485d343d46f8716fa6319e3acde3f
melihkocaadam <melihkocaadam@gmail.com>
2020-05-23 16:28:42 +0300

    edit

--- static/js/newReport.js
+++ static/js/newReport.js
@@ -77,7 +77,7 @@ function insertAllData(data, key) {
                     }
                 }
             } else {
-                allData[key] = [data];
+                allData[key] = data;
                 break;
             }
         }
