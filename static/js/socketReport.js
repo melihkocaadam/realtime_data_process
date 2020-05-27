40589d1252505afca954d86e371b600ea7775833
melihkocaadam <melihkocaadam@gmail.com>
2020-05-27 10:36:37 +0300

    add unauthorized handling

--- static/js/socketReport.js
+++ static/js/socketReport.js
@@ -21,7 +21,16 @@ function startStop() {
 
 function createConnSocket(topicName) {
     var socket = io();
+    
     socket.on("connect", function() {
         socket.emit(topicName, {data: "connected to " + topicName})
     });
+
+    socket.on("unauthorized", function(error) {
+        // this should now fire
+        if (error.data.type == "UnauthorizedError" || error.data.code == "invalid_token") {
+          alert("User's token has expired");
+        }
+    });
+
 }
