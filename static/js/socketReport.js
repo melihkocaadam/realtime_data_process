65b845827d8642edc22f0b7a8129c4f24c639936
melihkocaadam <melihkocaadam@gmail.com>
2020-05-28 11:37:36 +0300

    edit seq to time

--- static/js/socketReport.js
+++ static/js/socketReport.js
@@ -82,12 +82,12 @@ function createHTML(jsonData) {
     console.log("into createHTML function");
     var col = [];
     for (var i = 0; i < jsonData.length; i++) {
-        for (var key in jsonData[i]) {
-            if (col.indexOf(key) === -1) {
-                col.push(key);
+        for (var j = 0; j < jsonData[i]; j++) {
+            if (col.indexOf(jsonData[i][j]) === -1) {
+                col.push(jsonData[i][j]);
             }
             if (key == "sequence") {
-                console.log(i);
+                console.log(j);
             }
         }
     }
