81472d1a8fdcd727a91a3f919bf6dae48a7b92fe
melihkocaadam <melihkocaadam@gmail.com>
2020-05-29 18:55:11 +0300

    websocket closed

--- web_app.py
+++ web_app.py
@@ -351,5 +351,6 @@ if __name__ == "__main__":
     # t = Thread(target=run_schedule)
     # t.start()
     print("*** websocket beginning ***")
-    socketio.run(app=app, debug=True, host="0.0.0.0", port=5000)
+    # socketio.run(app=app, debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
     print("*** webapp started ***")
