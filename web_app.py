fa45ab549dfe650de6183dd6ea1276bd74c5fdb8
melihkocaadam <melihkocaadam@gmail.com>
2020-05-29 18:41:00 +0300

    fixed

--- web_app.py
+++ web_app.py
@@ -341,5 +341,6 @@ def run_schedule():
 if __name__ == "__main__":
     t = Thread(target=run_schedule)
     t.start()
+    print("*** websocket beginning ***")
     socketio.run(app=app, debug=True, host="0.0.0.0", port=5000)
     print("*** webapp started ***")
