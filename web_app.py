5712af51cbd10afd78786c331e91d1962149de44
melihkocaadam <melihkocaadam@gmail.com>
2020-05-29 18:57:17 +0300

    added schedule

--- web_app.py
+++ web_app.py
@@ -348,8 +348,8 @@ def run_schedule():
 ### Main Method ###
 ###################
 if __name__ == "__main__":
-    # t = Thread(target=run_schedule)
-    # t.start()
+    t = Thread(target=run_schedule)
+    t.start()
     print("*** websocket beginning ***")
     # socketio.run(app=app, debug=True, host="0.0.0.0", port=5000)
     app.run(debug=True, host="0.0.0.0", port=5000)
