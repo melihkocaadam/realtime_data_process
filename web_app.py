7810293ec057d030c7fa5aeec0148ae79c741bf8
melihkocaadam <melihkocaadam@gmail.com>
2020-05-29 19:05:23 +0300

    debug false

--- web_app.py
+++ web_app.py
@@ -351,6 +351,6 @@ if __name__ == "__main__":
     t = Thread(target=run_schedule)
     t.start()
     print("*** webapp beginning ***")
-    socketio.run(app=app, debug=True, host="0.0.0.0", port=5000)
-    # app.run(debug=True, host="0.0.0.0", port=5000)
+    socketio.run(app=app, debug=False, host="0.0.0.0", port=5000)
+    # app.run(debug=False, host="0.0.0.0", port=5000)
     print("*** webapp stoped ***")
