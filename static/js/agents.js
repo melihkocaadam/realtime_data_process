1af0caf2137af97580accd56fd72a966799ee179
melihkocaadam <melihkocaadam@gmail.com>
2020-06-11 17:17:09 +0300

    edit

--- static/js/agents.js
+++ static/js/agents.js
@@ -238,19 +238,15 @@ function auxClick(agent_name){
 function postAgentStatus(jsonData){
     var Url = "http://" + hostName + ":5000/sendAgentStatus";
 
-    var prevSequence = 0;
-
     var getExistAgents = JSON.parse(localStorage.getItem('agentList')) || [];
 
     if (getExistAgents != undefined || getExistAgents != '') {
         getExistAgents.forEach(function(agentRow) {
             if (agentRow["agent"] == jsonData["agent"] && !("prevSequence" in jsonData)) {
-                prevSequence = agentRow["sequence"];
+                jsonData["prevSequence"] = agentRow["sequence"];
             }
         });
     }
-
-    jsonData["prevSequence"] = prevSequence;
 
     $.ajax({
         url: Url,
